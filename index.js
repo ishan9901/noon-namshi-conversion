b517de0bc07d41dfdc59cc3939609ba6dcffbc48
ishan9901 <ishan.dhawane@namshi.com>
2023-02-27 12:03:15 +0530

    first version

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ const _ = require("lodash");
 
 const NoonToNamshiMap = reader.readFile('./Crosslisting ^ Mapping & Product Upload.xlsx');
 
-const Headers = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['Headers']);
-const CategoryMap = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['Category_Mapping'], { range: 1 });
-const catMap = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['CatMap'], { range: 1 });
-const GenderMap = _.keyBy(reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['Gender_Mapping']), "Noon - Gender");
+const Headers = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['HeadMap']);
+const CategoryMap = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['CatMap'], { range: 1 });
+let CountryMap = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['Country_Mapping'], { range: 1 });
+let ColorMap = reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['Colour_Mapping'],);
+
+CountryMap = CountryMap.reduce((acc, val) => {
+    acc[val["country_of_origin_1"]?.toLowerCase().trim()] = val["country_of_origin"]?.toLowerCase().trim();
+    return acc;
+}, {});
+
+ColorMap = ColorMap.reduce((acc, val) => {
+    acc[val["noon"]?.toLowerCase().trim()] = val["namshi"]?.toLowerCase().trim();
+    return acc;
+}, {});
+
+// const GenderMap = _.keyBy(reader.utils.sheet_to_json(NoonToNamshiMap.Sheets['Gender_Mapping']), "Noon - Gender");
+
+// console.log('Headers', Headers);
 
 // console.log('GenderMap', GenderMap);
 // console.log('CategoryMap', CategoryMap);
-console.log('catMap', catMap);
+// console.log('catMap', catMap);
+// console.log('ColorMap', ColorMap);
 
 const namshi_to_noon_headers = Headers.reduce((acc, val, index) => {
 
@@ -23,21 +38,19 @@ const namshi_to_noon_headers = Headers.reduce((acc, val, index) => {
 
 }, {});
 
-// console.log('Headers', Headers);
 // console.log('namshi_to_noon_headers', namshi_to_noon_headers);
 
-const file2 = reader.readFile('./noon-tested-file.xlsx');
-
-const noonTestedFile = reader.utils.sheet_to_json(file2.Sheets['MP Format'], { range: 1 });
-
-// console.log('Headers', Headers);
+// const file2 = reader.readFile('./noon-tested-file.xlsx');
+// const noonTestedFile = reader.utils.sheet_to_json(file2.Sheets['MP Format'], { range: 1 });
 // console.log('noonTestedFile', noonTestedFile);
 
-const file3 = reader.readFile('./GMG Product Upload.xlsx');
+const file3 = reader.readFile('./noon-data.xlsx');
 
 const noon_format = reader.utils.sheet_to_json(file3.Sheets['noon_format'], { range: 1 });
 
-// console.log('noon_format', noon_format);
+const file4 = reader.readFile('./size_runs.xlsx');
+
+const size_runs = reader.utils.sheet_to_json(file4.Sheets['size_runs']);
 
 
 const namshiHeaders = ["brand_key",
@@ -73,58 +86,69 @@ for (let i = 1; i <= 152; i++) {
 // console.log('namshiHeaders', JSON.stringify(namshiHeaders));
 
 
-const namshi_format = noon_format.map(noon => {
+let namshi_format = {};
+
+for (let noon of noon_format) {
 
     try {
+
+        const item = namshi_format[noon[namshi_to_noon_headers["product_id"]]];
+
+        const sizekey = getSize(noon);
+
+        if (item) {
+            item[sizekey] = noon.sku;
+            continue;
+        }
+
+        const categoryDetail = getCategoryData(noon);
+
         const namshiObj = {
             "brand_key": noon[namshi_to_noon_headers["brand_key"]],
             "supplier_sku": noon[namshi_to_noon_headers["supplier_sku"]],
             "product_id": noon[namshi_to_noon_headers["product_id"]],
-            "color": noon[namshi_to_noon_headers["color"]],
+            "color": ColorMap[noon[namshi_to_noon_headers["color"]]?.toLowerCase().trim()],
             "name_en": noon[namshi_to_noon_headers["name_en"]],
             "name_ar": noon[namshi_to_noon_headers["name_en"]],
             "short_description_en": noon[namshi_to_noon_headers["short_description_en"]],
             "short_description_ar": noon[namshi_to_noon_headers["short_description_ar"]],
             "specialist": noon[namshi_to_noon_headers["specialist"]],
-            "age_group": GenderMap[noon[namshi_to_noon_headers["gender"]].toLowerCase()].AGE_GROUP,
-            "gender": GenderMap[noon[namshi_to_noon_headers["gender"]].toLowerCase()].GENDER,
-            "department": noon[namshi_to_noon_headers["department"]],
-            "category": noon[namshi_to_noon_headers["category"]],
-            "sub_category": noon[namshi_to_noon_headers["sub_category"]],
-            "basic_type": noon[namshi_to_noon_headers["basic_type"]],
-            "occasion": noon[namshi_to_noon_headers["occasion"]],
-            "product_detail": noon[namshi_to_noon_headers["product_detail"]],
-            "country_of_origin": noon[namshi_to_noon_headers["country_of_origin"]],
+            "age_group": categoryDetail["age_group"],
+            "gender": categoryDetail["gender"],
+            "department": categoryDetail["department"],
+            "category": categoryDetail["category"],
+            "sub_category": categoryDetail["sub_category"],
+            "basic_type": categoryDetail["basic_type"],
+            "occasion": categoryDetail["occasion"],
+            "product_detail": categoryDetail["product_detail"],
+            "country_of_origin": CountryMap[noon[namshi_to_noon_headers["country_of_origin"]]?.toLowerCase().trim()],
             "warranty": noon[namshi_to_noon_headers["warranty"]],
             "dangerous_good_type": noon[namshi_to_noon_headers["dangerous_good_type"]],
             "images": noon[namshi_to_noon_headers["images"]],
             "unit_cost": noon[namshi_to_noon_headers["unit_cost"]],
             "original_price": noon[namshi_to_noon_headers["original_price"]],
-            "sizerun": noon[namshi_to_noon_headers["sizerun"]]
+            "sizerun": categoryDetail["sizerun"]
         };
 
-        const categoryDetail = getCategoryData(noon);
+        namshiObj[sizekey] = noon.sku;
 
-        // console.log('categoryDetail', categoryDetail);
+        namshi_format[namshiObj.product_id] = namshiObj;
 
-        return namshiObj;
     }
     catch (err) {
         console.log('err', err.message);
     }
 
-});
+};
 
-// console.log('namshiHeaders', namshiHeaders);
-
+// console.log('namshi_format', namshi_format);
 const colNames = namshiHeaders.reduce((acc, val) => {
     acc[val] = val;
     return acc;
 }, {});
 
+namshi_format = Object.values(namshi_format);
 namshi_format.unshift(colNames);
-
-console.log('namshi_format', namshi_format);
 
 const ws = reader.utils.json_to_sheet(namshi_format);
 
@@ -132,7 +156,7 @@ const newFile = reader.utils.book_new();
 
 reader.utils.book_append_sheet(newFile, ws, "namshi_format");
 
-reader.writeFile(newFile, './namshi_format.xlsx');
+reader.writeFile(newFile, './n2n-code-converted.xlsx');
 
 // Functions
 
@@ -140,12 +164,42 @@ function getCategoryData(noon) {
 
     const categoryDetail = CategoryMap.find(obj => {
 
-        if (obj.product_type == noon.product_type && obj.product_subtype == noon.product_subtype) return obj;
+        if (obj.department.toLowerCase() == noon.department.toLowerCase() &&
+            obj.gender.toLowerCase() == noon.gender.toLowerCase() &&
+            obj.family.toLowerCase() == noon.family.toLowerCase() &&
+            obj.product_type.toLowerCase() == noon.product_type.toLowerCase() &&
+            obj.product_subtype.toLowerCase() == noon.product_subtype.toLowerCase() &&
+            obj.basic_type.toLowerCase() == noon.basic_type.toLowerCase() &&
+            obj.occasion.toLowerCase() == noon.occasion.toLowerCase() &&
+            obj.pattern_type.toLowerCase() == noon.pattern_type.toLowerCase()
+        ) return obj;
 
-    })
+    });
 
-    return categoryDetail;
+    if (!categoryDetail) return {};
+
+    return {
+        "age_group": categoryDetail["age_group"],
+        "gender": categoryDetail["gender_1"],
+        "department": categoryDetail["department_1"],
+        "category": categoryDetail["category"],
+        "sub_category": categoryDetail["sub_category"],
+        "basic_type": categoryDetail["basic_type_1"],
+        "occasion": categoryDetail["occasion_1"],
+        "product_detail": categoryDetail["product_detail"],
+        "sizerun": categoryDetail["size_run"]
+    };
 
 }
 
+function getSize(noon) {
 
+    const sizeRun = size_runs.find(run => run.size_run.toLowerCase() == "MEN Tops XS-4XL".toLowerCase());
+
+    for (let size in sizeRun) {
+
+        if (sizeRun[size].toLowerCase() == noon.size.toLowerCase()) return size;
+
+    }
+
+};
